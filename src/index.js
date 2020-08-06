f94cfbb6d53683ca008cb6be3fc914e57897faad
Danial Javanmardi <danialjavanmardi@Danials-MacBook-Pro.local>
2020-08-06 02:09:01 -0700

    Add Collapsible states and buttons

--- src/index.js
+++ src/index.js
@@ -2,46 +2,24 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import jsonData from "./data.json";
+import Sidebar from "./sidebar"
 
 const DataURL =
   "https://gist.githubusercontent.com/Icehunter/3fde165a38380d4072f6bcb6726f1a63/raw/4dadf7d7c485255c9fdd0fa5afa5fbc10d79990a/countries+states+cities.json";
 
-const SidebarItem = ({data, depth=0}) => (
-    <div className="menuList">
-        {
-            data.map( ({name, id, ...props}) => {
-                return (<>
-                    <div className="menuListItem" key={id} style={{ paddingLeft: depth }} >
-                        <span className="ListItemText">{name}</span>
-                    </div>
-                    {props.states ? <SidebarItem data={props.states} depth={depth+20} /> : null }
-                    {props.cities ? <SidebarItem data={props.cities} depth={depth+20} /> : null }
-                </>)
-            })
-        }
-    </div>
-)
-
-const Sidebar = ({items}) => 
- {
-  return (
-    <div  className="sidebar">
-        <SidebarItem data={items} depth={0} />
-    </div>
-  )
-}
-
 const App = ({items=[]}) => {
-  // console.log(items);
+
   const [data, setData] = useState(items);
 
-  // useEffect(() => {
-  //   fetch(DataURL)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setData(data);
-  //     });
-  // });
+  /* 
+  useEffect(() => {
+    fetch(DataURL)
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+      });
+  }); 
+  */
   
   return (<>
     { (data.length > 0) ? <Sidebar items={data} /> : <span>Loading</span>}
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const SidebarItem = ({data, depth=0}) => (
-    <div className="menuList">
-        {
-            data.map( ({name, id, ...props}) => {
-                return (<>
-                    <div className="menuListItem" key={id} style={{ paddingLeft: depth }} >
-                        <span className="ListItemText">{name}</span>
-                    </div>
+const Collapsible = ({id, name, depth, props}) => {
+
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggle = () => setIsOpen(!isOpen);
+    return (<div className="menuListItem" >
+                <button key={id} onClick={toggle}  style={{ paddingLeft: depth }} className={ !isOpen ? "plus" : "minus"} >
+                    {name}
+                </button>
+                <div className={ !isOpen ? "collapsed" : ""}>
                     {props.states ? <SidebarItem data={props.states} depth={depth+20} /> : null }
                     {props.cities ? <SidebarItem data={props.cities} depth={depth+20} /> : null }
-                </>)
-            })
-        }
-    </div>
-)
+                </div>
+            </div>);
+}
+
+const SidebarItem = ({data, depth=0}) => 
+{
+    return (
+        <div className="menuList">
+            {
+                data.map( ({name, id, ...props}) => {
+                    return (<Collapsible id={id} name={name} depth={depth} props={props}/>)
+                })
+            }
+        </div>
+    )
+}
 
 const Sidebar = ({items}) => 
  {
@@ -24,6 +37,5 @@ const Sidebar = ({items}) =>
     </div>
   )
 }
-
 
 export default Sidebar
