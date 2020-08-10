0eca5175b1441a9be7b1194827b43ecace216d66
Danial Javanmardi <danialjavanmardi@Danials-MacBook-Pro.local>
2020-08-10 02:27:26 -0700

    add airbnb eslint

--- src/index.js
+++ src/index.js
@@ -1,53 +1,58 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
-import jsonData from "./data.json";
-import Sidebar from "./sidebar";
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import jsonData from './data.json';
+import Sidebar from './sidebar';
 
-const DataURL =
-  "https://raw.githubusercontent.com/mandanemedia/ListsOfCounties/master/src/countries%2Bstates%2Bcities.json";
+const DataURL = 'https://raw.githubusercontent.com/mandanemedia/ListsOfCounties/master/src/countries%2Bstates%2Bcities.json';
 
-const App = ({items=[]}) => {
-
+const App = ({ items = [] }) => {
   const [data, setData] = useState(items);
-  
+
   useEffect(() => {
     fetch(DataURL)
-      .then(res => res.json())
-      .then(data => {
-        setData(data);
+      .then((res) => res.json())
+      .then((newData) => {
+        setData(newData);
       });
-  },[]);
+  }, []);
 
   const handleRemoveDataRow = (parents) => {
-    //delete country 
-    if(parents.length === 1) {
+    // delete country
+    if (parents.length === 1) {
       let newData = [...data];
-      newData = newData.filter((item) =>  [parents[0]].indexOf( item.id ) === -1 )
+      newData = newData.filter(item => [parents[0]].indexOf(item.id) === -1);
       setData(newData);
     }
-    //delete state
-    else if(parents.length === 2){
-      let newData = [...data];
-      let countryIndex = newData.findIndex( row => row.id === parents[0]);
-      newData[countryIndex].states = newData[countryIndex].states.filter((item) =>  [parents[1]].indexOf( item.id ) === -1 );
+    // delete state
+    else if (parents.length === 2) {
+      const newData = [...data];
+      const countryIndex = newData.findIndex((row) => row.id === parents[0]);
+      newData[countryIndex].states = newData[countryIndex].states.filter(
+        (item) => [parents[1]].indexOf(item.id) === -1,
+      );
+      setData(newData);
+    } else if (parents.length === 3) {
+    // delete city
+      const newData = [...data];
+      const countryIndex = newData.findIndex((row) => row.id === parents[0]);
+      const stateIndex = newData[countryIndex].states.findIndex((row) => row.id === parents[1]);
+      newData[countryIndex].states[stateIndex].cities = newData[countryIndex]
+        .states[stateIndex].cities.filter((item) => [parents[2]].indexOf(item.id) === -1);
       setData(newData);
     }
-    //delete city
-    else if(parents.length === 3){
-      let newData = [...data];
-      let countryIndex = newData.findIndex( row => row.id === parents[0] );
-      let stateIndex = newData[countryIndex].states.findIndex( row => row.id === parents[1]);
-      newData[countryIndex].states[stateIndex].cities = newData[countryIndex].states[stateIndex].cities.filter((item) =>  [parents[2]].indexOf( item.id ) === -1 );
-      setData(newData);
-    }
-  }
+  };
 
-  return (<>
-    { (data.length > 0) ? <Sidebar items={data} onRemoveDataRow={handleRemoveDataRow} /> : <span>Loading</span>}
-  </>);
+  return (
+    <>
+      {(data.length > 0)
+        ? <Sidebar items={data} onRemoveDataRow={handleRemoveDataRow} />
+        : <span>Loading</span>}
+    </>
+  );
 };
 
 ReactDOM.render(
-  <App items={jsonData}/>
-, document.getElementById("root"));
+  <App items={jsonData} />,
+  document.getElementById('root'),
+);
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,64 +1,92 @@
 // @flow
-import React, {useState} from 'react'
-import {Collapse} from 'react-collapse';
-import { IoIosRemove, IoIosAdd } from "react-icons/io";
-import { AiFillDelete } from "react-icons/ai";
+import React, { useState } from 'react';
+import { Collapse } from 'react-collapse';
+import { IoIosRemove, IoIosAdd } from 'react-icons/io';
+import { AiFillDelete } from 'react-icons/ai';
 
 type itemType = {
     name: string,
     id: number,
     states? : Array<itemType>,
     cities? : Array<itemType>
-}
+};
 
-const Collapsible = ({id, name, depth, onRemoveDataRow, props}) => {
+const Collapsible = (
+  {
+    id, name, depth, onRemoveDataRow, props,
+  },
+) => {
+  const [isOpen, setIsOpen] = useState(true);
 
-    const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen(!isOpen);
+  const hasChild = (props.states && props.states.length > 0)
+  || (props.cities && props.cities.length > 0);
+  const parrents = props.parrents.length > 0 ? [...props.parrents, ...[id]] : [id];
+  const removeDataRow = (newParrents) => {
+    onRemoveDataRow(newParrents);
+  };
+  return (
+    <div className="menuListItem">
+      <div className="rowItem">
+        <AiFillDelete onClick={() => removeDataRow(parrents)} className="removeRow" />
+        <button type="button" style={{ marginLeft: depth }} onClick={toggle} className={`treeButton ${hasChild ? ' hasChild' : ' noChild'}`}>
+          {hasChild ? isOpen ? <IoIosRemove /> : <IoIosAdd /> : null}
+          {name}
+        </button>
+      </div>
+      {hasChild ? (
+        <Collapse isOpened={isOpen}>
+          {props.states
+            ? (
+              <SidebarItem
+                data={props.states}
+                depth={depth + 20}
+                onRemoveDataRow={onRemoveDataRow}
+                parrents={parrents}
+              />
+            ) : null }
+          {props.cities
+            ? (
+              <SidebarItem
+                data={props.cities}
+                depth={depth + 20}
+                onRemoveDataRow={onRemoveDataRow}
+                parrents={parrents}
+              />
+            ) : null }
+        </Collapse>
+      )
+        : null}
+    </div>
+  );
+};
 
-    const toggle = () => setIsOpen(!isOpen);
-    const hasChild = (props.states && props.states.length>0) || ( props.cities && props.cities.length>0);
-    const parrents = props.parrents.length > 0 ?  [...props.parrents, ...[id]] : [id];
-    const removeDataRow = (parrents) => {
-        onRemoveDataRow(parrents);
-    };
-    return (<div className="menuListItem" >
-                <div className="rowItem"> 
-                    <AiFillDelete onClick={()=>removeDataRow(parrents)} className="removeRow" />
-                    <button style={{ marginLeft: depth}} onClick={toggle} className={`treeButton ${hasChild ? " hasChild": " noChild" }`} >
-                        {hasChild ? isOpen ? <IoIosRemove /> : <IoIosAdd /> : null}
-                        {name}
-                    </button>
-                </div>
-                {hasChild ?  
-                <Collapse isOpened={isOpen}>
-                    {props.states ? <SidebarItem data={props.states} depth={depth+20} onRemoveDataRow={onRemoveDataRow} parrents ={parrents} /> : null }
-                    {props.cities ? <SidebarItem data={props.cities} depth={depth+20} onRemoveDataRow={onRemoveDataRow} parrents ={parrents} /> : null }
-                </Collapse> 
-                :null}
-            </div>);
-}
-
-const SidebarItem = ({data, depth=0, onRemoveDataRow=null, parrents=[] }:{data:Array<itemType>, depth:number, onRemoveDataRow:any, parrents: Array<any>}) => 
-{
-    return (
-        <div className="menuList">
-            {
-                data.map( ({name, id, ...props}) => {
-                    props = { ...props , ...{parrents}};
-                    return (<Collapsible key={id} id={id} name={name} depth={depth} onRemoveDataRow={onRemoveDataRow} props={props}/>)
+const SidebarItem = ({
+  data, depth = 0, onRemoveDataRow = null, parrents = [],
+}:{data:Array<itemType>, depth:number, onRemoveDataRow:any, parrents: Array<any>}) => (
+  <div className="menuList">
+    {
+                data.map(({ name, id, ...props }) => {
+                  const newprops = { ...props, ...{ parrents } };
+                  return (
+                    <Collapsible
+                      key={id}
+                      id={id}
+                      name={name}
+                      depth={depth}
+                      onRemoveDataRow={onRemoveDataRow}
+                      props={newprops}
+                    />
+                  );
                 })
             }
-        </div>
-    )
-}
+  </div>
+);
 
-const Sidebar = ({items, onRemoveDataRow}:{items:Array<itemType>, onRemoveDataRow:any}) => 
- {
-  return (
-    <div  className="sidebar">
-        <SidebarItem data={items} depth={0} onRemoveDataRow={onRemoveDataRow} parrents={[]}/>
-    </div>
-  )
-}
+const Sidebar = ({ items, onRemoveDataRow }:{items:Array<itemType>, onRemoveDataRow:any}) => (
+  <div className="sidebar">
+    <SidebarItem data={items} depth={0} onRemoveDataRow={onRemoveDataRow} parrents={[]} />
+  </div>
+);
 
-export default Sidebar
+export default Sidebar;
